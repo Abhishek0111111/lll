7b426dcfbcc81b03fb1b85616227ba0c4e66c9de
abcrr <abcrr@hdd>
2023-12-06 11:32:17 +0530

    hiii

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <html>
+      <body>
+        <h1>
+          hello world
+        </h1>
+      </body>
+    </html>
+  );
+}
+
+export default App;
